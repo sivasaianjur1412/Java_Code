0bb3974aae3c1b41e312ab155425e5c43fd29752
sivasaianjur1412 <sivasaianjur1412@gmail.com>
2024-01-31 16:00:28 -0600

    additional programming questions

---
+++ src/main/java/additional/Programs.java
@@ -0,0 +1,174 @@
+package additional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Programs {
+
+    public int reverseNumber(int number) {
+        int reverse = 0;
+        while(number > 0) {
+
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number /= 10;
+        }
+        return reverse;
+    }
+
+    /***
+     * Palindrome of a number
+     * @param number
+     * @return
+     */
+    public boolean isPalindrome(int number) {
+        return reverseNumber(number) == number;
+    }
+
+    /***
+     * Swapping two numbers without using the third variable
+     */
+    public int[] swappingWithOutThird(int numberOne, int numberTwo) {
+        numberOne = numberOne * numberTwo;
+        numberTwo = numberOne / numberTwo;
+        numberOne /= numberTwo;
+        return new int[]{numberOne, numberTwo};
+    }
+
+    /***
+     * Factorial of a number
+     * @param number
+     * @return
+     */
+
+    public int getFactorial(int number) {
+        int factorialNumber = 1;
+        for(int i = number; i >= 1; i--) {
+            factorialNumber *= i;
+        }
+        if(number < 0)
+            return -1;
+        return factorialNumber;
+    }
+
+    /***
+     * Repeating characters of a string.
+     * @param input
+     * @return
+     */
+
+    public ArrayList<Character> getRepeatedCharacters(String input) {
+        ArrayList<Character> characterArrayList = new ArrayList<>();
+        char[] charArray = input.toCharArray();
+        HashMap<Character, Integer> characterIntegerHashMap = new HashMap<>();
+        for (char c : charArray) {
+            if (characterIntegerHashMap.containsKey(c)) {
+                characterIntegerHashMap.put(c, characterIntegerHashMap.get(c) + 1);
+            } else characterIntegerHashMap.put(c, 1);
+        }
+        for(int i = 0; i < characterIntegerHashMap.size(); i++) {
+            if(characterIntegerHashMap.get(charArray[i]) > 1)
+                characterArrayList.add(charArray[i]);
+        }
+        return characterArrayList;
+    }
+
+    /***
+     * Getting the count of unique words in a string.
+     * @param input
+     * @return
+     */
+
+    public int getCountUniqueWords(String input) {
+        HashMap<String, Integer> stringIntegerHashMap = new HashMap<>();
+        if(input.isEmpty())
+            return 0;
+        String[] stringArray = input.split(" ");
+        for(int i = 0; i < stringArray.length; i++) {
+            if(stringIntegerHashMap.containsKey(stringArray[i])) {
+                stringIntegerHashMap.put(stringArray[i], stringIntegerHashMap.get(stringArray[i]) + 1);
+            }
+            else stringIntegerHashMap.put(stringArray[i], 1);
+        }
+        int count = 0;
+        for(int i = 0; i < stringIntegerHashMap.size(); i++) {
+            if(stringIntegerHashMap.get(stringArray[i]) == 1)
+                count++;
+        }
+        return count;
+    }
+
+    /***
+     * Check whether one string is rotated of another.
+     * @param inputOne
+     * @param inputTwo
+     * @return
+     */
+    public boolean isStringRotated(String inputOne, String inputTwo) {
+        String output = inputOne + inputOne;
+        return output.contains(inputTwo);
+    }
+
+
+    /***
+     * Sorted Hash map
+     * @param hashMap
+     * @return
+     */
+    public Map<Integer, String> sortHashMap(Map<Integer, String> hashMap) {
+        return new TreeMap<>(hashMap);
+    }
+
+
+    /***
+     * Reversing the array list using loop.
+     * @param integerArrayList
+     * @return
+     */
+    public ArrayList<Integer> getReverse(ArrayList<Integer> integerArrayList) {
+        ArrayList<Integer> outputArrayList = new ArrayList<>();
+        for(int i = integerArrayList.size() - 1; i >= 0; i--)
+            outputArrayList.add(integerArrayList.get(i));
+
+        return outputArrayList;
+    }
+
+
+    /***
+     * Print sum of all the items of the array.
+     * Time complexity will be O(n).
+     * @param arr
+     * @return
+     */
+    public int getSumItems(int[] arr) {
+        int sum = 0;
+        for(int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    /***
+     * Replace the lower case characters to upper case and vice versa.
+     * Time complexity will be O(n).
+     * @param input
+     * @return
+     */
+    public String convertString(String input) {
+        String outputString = "";
+        char[] charArray = input.toCharArray();
+        for (char c : charArray) {
+            if (Character.isLowerCase(c)) {
+                outputString += Character.toUpperCase(c);
+            } else if (Character.isUpperCase(c))
+                outputString += Character.toLowerCase(c);
+            else
+                outputString += c;
+
+        }
+        return outputString.toString();
+    }
+
+}
---
+++ src/test/java/additional/ProgramsTest.java
@@ -0,0 +1,123 @@
+package additional;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProgramsTest {
+    Programs programs = new Programs();
+
+    /***
+     * Success and Failure case
+     */
+    @Test
+    public void testPalindrome() {
+        Assert.assertTrue(programs.isPalindrome(12321));
+        Assert.assertFalse(programs.isPalindrome(1231));
+    }
+
+    /***
+     * Testing the swap of two numbers
+     */
+    @Test
+    public void testSwapping() {
+        Assert.assertArrayEquals(new int[]{8, 9}, programs.swappingWithOutThird(9, 8));
+    }
+
+    /***
+     * Testing Factorial
+     */
+    @Test
+    public void testFactorial() {
+        Assert.assertEquals(120, programs.getFactorial(5));
+        Assert.assertNotEquals(120, programs.getFactorial(4));
+        Assert.assertEquals(1, programs.getFactorial(0));
+        Assert.assertEquals(-1, programs.getFactorial(-121));
+
+    }
+
+    /***
+     * Testing repeated characters.
+     */
+    @Test
+    public void testCharacterCount() {
+        ArrayList<Character> arrayList = new ArrayList<>(Arrays.asList('c', 'a'));
+        Assert.assertEquals(arrayList, programs.getRepeatedCharacters("canaca"));
+    }
+
+    /***
+     * Testing unique word count.
+     */
+    @Test
+    public void testGetUniqueWords() {
+        Assert.assertEquals(6, programs.getCountUniqueWords("This is a sample string and string have characters"));
+        //Failure
+        Assert.assertNotEquals(3, programs.getCountUniqueWords("shiva"));
+        //Edge case
+        Assert.assertEquals(0, programs.getCountUniqueWords(""));
+    }
+
+    /***
+     * Checking the string rotation.
+     */
+    @Test
+    public void testStringRotation() {
+        Assert.assertTrue(programs.isStringRotated("shiva123", "123shiva"));
+    }
+
+    /***
+     * Testing the sum of all elements in an array.
+     */
+    @Test
+    public void testGetSumItems() {
+        Assert.assertEquals(15, programs.getSumItems(new int[]{1,2,3,4,5}));
+        Assert.assertNotEquals(2, programs.getSumItems(new int[]{1,2,3,4}));
+    }
+
+    /***
+     * Testing the conversion of a string.
+     */
+    @Test
+    public void testConvertingString() {
+        Assert.assertEquals("heLLO", programs.convertString("HEllo"));
+        Assert.assertEquals("Siva.", programs.convertString("sIVA."));
+        Assert.assertNotEquals("shiva", programs.convertString("shiva"));
+    }
+
+    /***
+     * Testing the hash map.
+     */
+    @Test
+    public void testSortHashMap() {
+        Map<Integer, String> hashMap = createHashMap();
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Sai");
+        map.put(2, "Shiva");
+        map.put(3, "A");
+        Assert.assertEquals(map, programs.sortHashMap(hashMap));
+    }
+
+    /***
+     * Testing reverse of an array list.
+     */
+    @Test
+    public void testReverseArrayList() {
+        ArrayList<Integer> integerArrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        ArrayList<Integer> outputArrayList = new ArrayList<>(Arrays.asList(5, 4, 3, 2, 1));
+
+        Assert.assertEquals(outputArrayList, programs.getReverse(integerArrayList));
+    }
+
+    public Map<Integer, String> createHashMap() {
+        Map<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(2, "Shiva");
+        hashMap.put(1, "Sai");
+        hashMap.put(3, "A");
+        return hashMap;
+    }
+
+}
